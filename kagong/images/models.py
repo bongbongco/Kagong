ec7c62f5d2e9cd2ed09cfbf4c648e7054f3c02e6
seungyonglee <bongbongco@gmail.com>
2018-02-16 09:48:27 +0900

    Images 앱 Like 모델 추가 및 Image, Comment, Like 모델 ForeignKey 추가

--- kagong/images/models.py
+++ kagong/images/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from kagong.users import models as user_models
+
 
 class TimeStampedModel(models.Model):
 
@@ -11,11 +13,26 @@ class TimeStampedModel(models.Model):
     
 class Image(TimeStampedModel):
 
+    """ Image Model """ 
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
 
 
 class Comment(TimeStampedModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
