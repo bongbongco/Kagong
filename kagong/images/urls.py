9c701825f24c76bcad9607720b369feb6eff2dec
seungyonglee <bongbongco@gmail.com>
2018-02-17 06:37:15 +0900

    유저 피드 만들기

--- kagong/images/urls.py
+++ kagong/images/urls.py
@@ -3,18 +3,8 @@ from . import views
 
 urlpatterns = [
     url(
-        regex=r'^all/$',
-        view=views.ListAllImages.as_view(),
-        name='all_images'
-    ),
-    url(
-        regex=r'^comment/all/$',
-        view=views.ListAllComment.as_view(),
-        name='all_comment'
-    ),
-    url(
-        regex=r'^like/all/$',
-        view=views.ListAllLike.as_view(),
-        name='all_like'
+        regex=r'^$',
+        view=views.Feed.as_view(),
+        name='feed'
     ),
 ]
