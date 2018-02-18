b9b8bf3b52d4848124655a0c3d01ffe9ef6e7ce5
seungyonglee <bongbongco@gmail.com>
2018-02-18 17:36:26 +0900

    비밀번호 변경

--- kagong/users/serializers.py
+++ kagong/users/serializers.py
@@ -6,6 +6,9 @@ from kagong.images import serializers as images_serializers
 class UserProfileSerializer(serializers.ModelSerializer):
 
     images = images_serializers.CountImageSerializer(many=True)
+    post_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     class Meta:
         model = models.User
