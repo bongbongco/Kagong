e9494f084493e85611dd31306789295526f90078
seungyonglee <bongbongco@gmail.com>
2018-02-21 04:30:56 +0900

    리액트 라우트 적용 및 리액토트론 연결 설정

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,36 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 import users from 'redux/modules/users';
+import Reactotron from 'ReactotronConfig';
+
+
+const env = process.env.NODE_ENV;
+
+const history = createHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
 
 const reducer = combineReducers({
-    users
-})
+    users,
+    routing: routerReducer
+});
 
-let store = initialState => createStore(reducer);
+let store;
+
+if(env === 'development') {
+    const {logger} = require('redux-logger');
+    middlewares.push(logger);
+    store = initialState => Reactotron.createStore(
+        reducer, applyMiddleware(...middlewares)
+    );
+} else {
+    store = initialState => createStore(
+        reducer, applyMiddleware(...middlewares)
+);
+}
+
+export { history };
 
 export default store();
