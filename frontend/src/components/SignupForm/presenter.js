692355ff40589957a4e37f248b33333c81fa2924
seungyonglee <bongbongco@gmail.com>
2018-03-07 07:25:31 +0900

    로그인 폼 입력 컨트롤 적용

--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -7,17 +7,44 @@ import formStyles from "components/shared/formStyles.scss";
 const SignupForm = (props, context) => (
     <div className={formStyles.formComponent}>
         <h3 className={formStyles.signupHeader}>{context.t("친구들에게 사진과 영상을 공유하세요.")}</h3>
-        <botton className={formStyles.button}>
+        <button className={formStyles.button}>
             <Ionicon icon="logo-facebook" fontSize="20px" color="white" />
                 {context.t("페이스북으로 로그인")}
-        </botton>
+        </button>
         <span className={formStyles.divider}>{context.t("혹은")}</span>
-        <form className={formStyles.form}>
-            <input type="email" placeholder={context.t("전자우편")} className={formStyles.textInput} />
-            <input type="text" placeholder={context.t("이름")} className={formStyles.textInput} />
-            <input type="username" placeholder={context.t("아이디")} className={formStyles.textInput} />
-            <input type="password" placeholder={context.t("패스워드")} className={formStyles.textInput} />
-            <input type="submit" value={context.t("가입")} className={formStyles.button} />
+        <form className={formStyles.form} onSubmit={props.handleSubmit}>
+            <input type="email" 
+                placeholder={context.t("전자우편")} 
+                className={formStyles.textInput} 
+                onChange={props.handleInputChange}
+                name="email"
+                value={props.emailValue}
+            />
+            <input type="text" 
+                placeholder={context.t("이름")} 
+                className={formStyles.textInput} 
+                onChange={props.handleInputChange}
+                name="fullname"
+                value={props.fullnameValue}
+            />
+            <input type="username" 
+                placeholder={context.t("아이디")} 
+                className={formStyles.textInput} 
+                onChange={props.handleInputChange}
+                name="username"
+                value={props.usernameValue}
+            />
+            <input type="password" 
+                placeholder={context.t("패스워드")} 
+                className={formStyles.textInput} 
+                onChange={props.handleInputChange}
+                name="password"
+                value={props.passwordValue}
+            />
+            <input type="submit" 
+                value={context.t("가입")} 
+                className={formStyles.button} 
+            />
         </form>
         <p className={formStyles.terms}>
             {context.t("동의 후 가입")}<br/> 
@@ -28,6 +55,15 @@ const SignupForm = (props, context) => (
 
 SignupForm.contextTypes = {
     t: PropTypes.func.PropTypes
+};
+
+SignupForm.PropTypes = {
+    handleInputChange: PropTypes.func.isRequired,
+    emailValue: PropTypes.string.isRequired,
+    fullnameValue: PropTypes.string.isRequired,
+    usernameValue: PropTypes.string.isRequired,
+    passwordValue: PropTypes.string.isRequired,
+    handleSubmit: PropTypes.func.isRequired
 };
 
 export default SignupForm;
