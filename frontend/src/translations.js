ed90b07d49c02f50ad199a6c34365596518caaf0
seungyonglee <bongbongco@gmail.com>
2018-03-06 21:04:16 +0900

    i18n_extract, i18n_import 적용

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,28 +1,28 @@
 export const translations = {
-    en: {
-        "로그인": "Login",
-        "카공 정보":"About Us",
-        "지원":"Support",
-        "블로그":"Blog",
-        "홍보":"Press",
-        "개인정보처리방침":"Privacy",
-        "약관":"Terms",
-        "언어":"Language",
-        "이름":"Full Name",
-        "아이디":"Username",
-        "패스워드":"Password",
-        "전자우편":"Email",
-        "혹은":"OR",
-        "페이스북으로 로그인":"Log in with Facebook",
-        "패스워드를 잊었다면":"Forgot password?",
-        "친구들에게 사진과 영상을 공유하세요.":"Sign up to see photo and videos from your friends.",
-        "가입":"Sign up",
-        "이미 가입하셨나요?":"Have an account?",
-        "아직 가입하지 않으셨나요?":"Don't have an account?",
-        "앱 설치하기":"Get the app",
-        "동의 후 가입":"By signing up, you agree to our ",
-        "이용약관 및 개인정보 처리방침":"Terms & Privacy Policy"
-    }
-};
-
- 
+  'en': {
+    '이미 가입하셨나요?': 'Have an account?',
+    '로그인': 'Log in',
+    '아직 가입하지 않으셨나요?': 'Don\'t have an account?',
+    '가입': 'Sign up',
+    '앱 설치하기': 'Get the app',
+    '아이디': 'Username',
+    '패스워드': 'Password',
+    '혹은': 'OR',
+    '페이스북으로 로그인': 'Log in with Facebook',
+    '패스워드를 잊었다면': 'Forgot password?',
+    '친구들에게 사진과 영상을 공유하세요.': 'Sign up to see photo and videos from your friends.',
+    '전자우편': 'Email',
+    '이름': 'Full name',
+    '동의 후 가입': 'By signing up, you agree to our',
+    '이용약관 및 개인정보 처리방침': 'Terms & Privacy Policy',
+    '카공 정보': 'About Us',
+    '지원': 'Support',
+    '블로그': 'Blog',
+    '홍보': 'Press',
+    '개인정보처리방침': 'Privacy',
+    '약관': 'Terms',
+    '언어': 'Language',
+  },
+  'options': {
+  },
+}
