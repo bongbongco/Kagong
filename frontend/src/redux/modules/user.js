c5d4bc4a8e06b7619ad1b9cbe23f9c89c29ef3ca
seungyonglee <bongbongco@gmail.com>
2018-03-16 12:52:28 +0900

    팔로우 언팔로우 API 서버 연결

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -135,17 +135,45 @@ function getPhotoLikes(photoId){
 
 function followUser(userId){
     return (dispatch, getState) => {
-        const { user: { token } } = getState();
         dispatch(setFollowUser(userId));
+        const { user: { token } } = getState();
+        fetch(`/users/${userId}/follow/`,{
+            method: "POST",
+            headers: {
+                Authorization: `JWT ${token}`,
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            if(response.status === 401){
+                dispatch(logout());
+            } else if (!response.ok) {
+                dispatch(setUnfollowUser(userId));
+            }
+        });
     };
-}
+};
 
 function unfollowUser(userId){
     return (dispatch, getState) => {
-        const { user: { token } } = getState();
         dispatch(setUnfollowUser(userId));
+        const { user: { token } } = getState();
+        fetch(`/users/${userId}/unfollow/`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `JWT ${token}`,
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            if(response.status === 401){
+                dispatch(logout());
+            } else if (!response.ok) {
+                dispatch(setFollowUser(userId));
+            }
+        });
     };
-}
+};
 // initial state
 
 const initialState = {
